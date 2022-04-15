542f4205716ac93457a145e90a71f2f092131679
Robinson Idagiza <robinadoro@gmail.com>
2022-04-15 15:24:19 +0300

    ft:Add a function that permits the default activity to be overriden

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function saturdayFun (){
+    const skater='roller-skate'
+    const dog = "bathe my dog"
+    
+    // return (`This Saturday, I want to ${skater}!`);
+    return `This Saturday, I want to ${dog}!`
+
+    
+}
+saturdayFun()
